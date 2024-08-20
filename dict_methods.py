884931e1394b2231b123856a0dfd453da825f12d
yuyu <swharriet2012@gmail.com>
2024-08-20 15:50:04 +1000

    original

---
+++ dict_methods.py
@@ -0,0 +1,19 @@
+
+# Dictionary Methods Exercise
+# I've provided you with a start dictionary called inventory. 
+
+inventory = {'croissant': 19, 'bagel': 4, 'muffin': 8, 'cake': 1} #DON'T CHANGE THIS LINE! 
+
+# Follow the instructions found in the comments:
+
+# 1. Make a copy of inventory  and save it to a variable called stock_list  using a dictionary method we've covered
+stock_list = dict(inventory)
+print(stock_list)
+
+# 2. Add the value 18 to stock_list  under the key "cookie"
+stock_list.update({'cookie': 18})
+print(stock_list)
+
+# 3. Remove 'cake' from stock_list  using a dictionary method we've learned
+stock_list.pop('cake')
+print(stock_list)
